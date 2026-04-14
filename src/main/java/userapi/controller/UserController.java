6aa62b0eea765f70a25410df0913ff76390e6aa2
Arthur Matos <274914017+Thurs3003@users.noreply.github.com>
2026-04-14 18:30:48 -0300

    feat: implementa CRUD básico de usuários com JPA

--- src/main/java/userapi/controller/UserController.java
+++ src/main/java/userapi/controller/UserController.java
@@ -2,19 +2,57 @@ package userapi.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import userapi.entity.User;
+import userapi.repository.UserRepository;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
+	
+	@Autowired
+	private UserRepository userRepository;
 
-	@GetMapping("/users")
+	@GetMapping
 	public List<User> listarUsuarios() {
-		return List.of(
-			new User(1L, "Arthur", "arthur@email.com", 20), 
-			new User(2L, "Maria", "maria@email.com", 22)
-		);
+		return userRepository.findAll();
+	}
+	
+	@PostMapping
+	public User criarUsuario(@RequestBody User user) {
+		return userRepository.save(user);
+	}
+	
+	@GetMapping("/{id}")
+	public User buscarPorId(@PathVariable Long id) {
+		return userRepository.findById(id).orElse(null);
+	}
+	
+	@PutMapping("/{id}")
+	public User atualizarUsuario(@PathVariable Long id, @RequestBody User userAtualizado) {
+		User user = userRepository.findById(id).orElse(null);
+		
+		if (user == null) {
+			return null;
+		}
+		
+		user.setNome(userAtualizado.getNome());
+		user.setEmail(userAtualizado.getEmail());
+		user.setIdade(userAtualizado.getIdade());
+		
+		return userRepository.save(user);
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	public String deletarUsuario(@PathVariable Long id) {
+		if (!userRepository.existsById(id)) {
+			return "Usuário não encontado.";
+		}
+		
+		userRepository.deleteById(id);
+		return "Usuário deletado com sucesso.";
 	}
 }
